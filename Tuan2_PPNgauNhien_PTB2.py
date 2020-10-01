b764ce3bac81a6cb2851196732936b16f46f7a97
PhamVietXuan <PhamVietXuan2000@gmail.com>
2020-10-01 21:26:29 +0700

    first commit

---
+++ Tuan2_PPNgauNhien_PTB2.py
@@ -0,0 +1,7 @@
+#Tính nghiệm của phương trình bậc hai bằng phương pháp ngẫu nhiên
+#input: hệ số a, b c của phương trình aX^2 + bX + c =0
+#Output: Nghiệm của phương trình bậc hai
+
+def phuong_trinh_b2(a, b, c):#he so a, b, c cua phuong trinh aX^2 + bX + c = 0
+    
+    pass
